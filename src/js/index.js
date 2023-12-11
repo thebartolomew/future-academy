c53864479952feb1c6bc5dfde98fb44aca52cb15
thebartolomew <tima.0704@mail.ru>
2023-12-12 02:23:56 +0600

    ended main page, added login page

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,14 @@
 Fancybox.bind()
+
+
+$('.age__faculty').on('click', 'div:not(.active)', function(){
+
+    $(this).addClass('active').siblings().removeClass('active')
+    .closest('div.age__video-block').find('div.age__video').removeClass('active').eq($(this).index()).addClass('active');
+
+    
+
+})
+
+
+console.log(document.querySelector('.age__video-block'));
